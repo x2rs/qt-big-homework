987ed0249752ded0447fe76c090e46fd1f0e52d0
hujunkai000 <2282776800@qq.com>
2024-06-20 16:08:15 +0800

    init 2

---
+++ build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Qt-666_FILE D:/Qtbox/Qt-666/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/Qt-666.exe)
+set(__QT_DEPLOY_TARGET_Qt-666_RUNTIME_DLLS D:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
